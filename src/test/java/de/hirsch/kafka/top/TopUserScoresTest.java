6b1537050e5184c71250ad139855b70f304bc6d2
DarkDeer <bruno.hirsch@gmx.de>
2017-11-27 18:34:00 +0100

    test

--- src/test/java/de/hirsch/kafka/top/TopUserScoresTest.java
+++ src/test/java/de/hirsch/kafka/top/TopUserScoresTest.java
@@ -1,10 +1,15 @@
 package de.hirsch.kafka.top;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.kstream.internals.WindowedDeserializer;
+import org.apache.kafka.streams.kstream.internals.WindowedSerializer;
 import org.apache.kafka.test.ProcessorTopologyTestDriver;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,26 +36,29 @@ public class TopUserScoresTest {
 	
 	@Test
 	public void test() {
+		final Serde<Windowed<String>> windowedStringSerde = Serdes.serdeFrom(new WindowedSerializer<>(stringSerde.serializer()), 
+				new WindowedDeserializer<>(stringSerde.deserializer())); 
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
 		String g1 = "g1";
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u1", g1), 10L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u2", g1), 20L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u3", g1), 30L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u4", g1), 40L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u5", g1), 50L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u6", g1), 60L, userGameSerde.serializer(), longSerde.serializer());
-		driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u7", g1), 70L, userGameSerde.serializer(), longSerde.serializer());
-		
-		ProducerRecord<String, String> output = driver.readOutput(TopUserScores.TOP_GAME_USERS_STREAM_NAME, stringSerde.deserializer(), stringSerde.deserializer());
-		
-		Assert.assertEquals(g1, output.key());
-		Assert.assertEquals("u7\nu6\nu5\nu4\nu3\n", output.value());
-	}
-	
-	@Test
-	public void testUserGameJson() {
-		UserGame ug = new UserGame("u1", "g1");
-		byte [] bytes = userGameSerde.serializer().serialize(null, ug);
-		Object o = userGameSerde.deserializer().deserialize(null, bytes);
-		Assert.assertEquals(ug, o);
+		final int n = 120;
+		long now = System.currentTimeMillis();
+		now = now - (now%(TopUserScores.TIME_WINDOW_SECONDS*1000L));
+		for (int i = 0; i < n; i++) {
+			
+			driver.process(TopUserScores.USER_SCORE_STREAM_NAME, new UserGame("u"+i, g1), 10L*i+1, userGameSerde.serializer(), longSerde.serializer(), now+(i*1000));
+
+			ProducerRecord<Windowed<String>, String> output = driver.readOutput(TopUserScores.TOP_GAME_USERS_STREAM_NAME, windowedStringSerde.deserializer(), stringSerde.deserializer());
+
+			Assert.assertEquals(g1, output.key().key());
+			StringBuilder sb = new StringBuilder();
+			for (int j = i; j >= Math.max(i - i%TopUserScores.TIME_WINDOW_SECONDS, i - 4); j--) {
+				sb.append("u").append(j).append('\n');
+			}
+			System.out.println(i+" window: "+sdf.format(output.key().window().start())+","+sdf.format(output.key().window().end()));
+			System.out.println(i+" : "+output.value());
+			Assert.assertEquals(" window "+i+" "+sdf.format(output.key().window().start()), now + (i/TopUserScores.TIME_WINDOW_SECONDS)*TopUserScores.TIME_WINDOW_SECONDS*1000L, output.key().window().start());
+			Assert.assertEquals("Output "+i, sb.toString(), output.value());
+			
+		}
 	}
 }
